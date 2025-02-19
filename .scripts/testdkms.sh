e48d87e8b7030546c53a238f711e5320685c1505
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-02-19 17:59:42 +0100

    Enhance testdkms.sh to copy .deb files to pkgs directory for improved package handling

--- .scripts/testdkms.sh
+++ .scripts/testdkms.sh
@@ -4,6 +4,8 @@ mkdir -p pkgs
 mv *-debpkg/*.deb pkgs/
 apt -y update
 ERRCODE=0
+
+find . -name "*.deb" -exec cp {} ./pkgs/ \;
 apt -y install ./pkgs/* || ERRCODE=$?
 find /var/lib/dkms/ -iname "make.log" -exec grep -H . {} \;
 exit ${ERRCODE}
