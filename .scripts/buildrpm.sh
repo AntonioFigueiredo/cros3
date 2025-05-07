3ed745c4dfaafee847ff2d273c943e92668dcc80
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-05-07 21:32:46 +0200

    test

--- .scripts/buildrpm.sh
+++ .scripts/buildrpm.sh
@@ -1,50 +1,29 @@
-# #!/bin/bash -x
-# set -e
-# #dnf install -y epel-release
-
-# KERNEL_VER=$(uname -r)
-# #dnf install -y "kernel-devel-${KERNEL_VER}"
-# dnf install -y kernel-devel kernel-headers
-# dnf install -y gcc make rpm-build rpmdevtools dkms dnf-utils kernel-abi-stablelists kernel-rpm-macros udev
-
-# rpmdev-setuptree
-
-# VERSION="0.1"
-
-# mv source_dir/src cros3-${VERSION}
-# tar cjf cros3-${VERSION}.tar.bz2 cros3-${VERSION}
-# cp *.tar.bz2 ~/rpmbuild/SOURCES/
-# cp source_dir/*.spec .
-
-# dnf builddep -y cros3.spec
-
-# rpmbuild -ba cros3.spec
-
-# cd ~
-# find
-# ls -la
-
-# mkdir -p ${GITHUB_WORKSPACE}/rpm-artifacts
-# cp -r ~/rpmbuild/RPMS ${GITHUB_WORKSPACE}/rpm-artifacts/
-# cp -r ~/rpmbuild/SRPMS ${GITHUB_WORKSPACE}/rpm-artifacts/
-
-#!/bin/bash
+#!/bin/bash -x
 set -e
+#dnf install -y epel-release
 
-# Set RPM build directories
-RPMBUILD_DIR=~/rpmbuild
-mkdir -p "$RPMBUILD_DIR"/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+#KERNEL_VER=$(uname -r)
+#dnf install -y "kernel-devel-${KERNEL_VER}"
+dnf install -y kernel-devel kernel-headers
+dnf install -y gcc make rpm-build rpmdevtools dkms dnf-utils kernel-abi-stablelists kernel-rpm-macros udev
 
-# Version must match spec file
-VERSION=0.1
-ARCHIVE_NAME="cros3-${VERSION}.tar.gz"
+rpmdev-setuptree
 
-# Create source tarball using git
-echo "Creating source archive $ARCHIVE_NAME..."
-git archive --format=tar.gz --prefix="cros3-${VERSION}/" -o "$RPMBUILD_DIR/SOURCES/$ARCHIVE_NAME" HEAD
+VERSION="0.1"
 
-# Copy the .spec file to SPECS
-cp cros3.spec "$RPMBUILD_DIR/SPECS/"
+mv source_dir/src cros3-${VERSION}
+tar cros3-${VERSION}.tar.bz2 cros3-${VERSION}
+cp *.tar.bz2 ~/rpmbuild/SOURCES/
+cp source_dir/*.spec .
 
-# Build the RPM
-rpmbuild -ba "$RPMBUILD_DIR/SPECS/cros3.spec"
+dnf builddep -y cros3.spec
+
+rpmbuild -ba cros3.spec
+
+#cd ~
+#find
+#ls -la
+
+mkdir -p ${GITHUB_WORKSPACE}/rpm-artifacts
+cp -r ~/rpmbuild/RPMS ${GITHUB_WORKSPACE}/rpm-artifacts/
+cp -r ~/rpmbuild/SRPMS ${GITHUB_WORKSPACE}/rpm-artifacts/
