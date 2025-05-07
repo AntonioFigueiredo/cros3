1765763f3beee6f9f4c6a25b627a8b03e133d307
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-05-07 21:15:17 +0200

    test

--- .scripts/buildrpm.sh
+++ .scripts/buildrpm.sh
@@ -1,18 +1,39 @@
-#!/bin/bash -x
+# #!/bin/bash -x
 set -e
-#dnf install -y epel-release
+# #dnf install -y epel-release
 
-KERNEL_VER=$(uname -r)
-#dnf install -y "kernel-devel-${KERNEL_VER}"
-dnf install -y kernel-devel kernel-headers
-dnf install -y gcc make rpm-build rpmdevtools dkms dnf-utils kernel-abi-stablelists kernel-rpm-macros udev
+# KERNEL_VER=$(uname -r)
+# #dnf install -y "kernel-devel-${KERNEL_VER}"
+# dnf install -y kernel-devel kernel-headers
+# dnf install -y gcc make rpm-build rpmdevtools dkms dnf-utils kernel-abi-stablelists kernel-rpm-macros udev
 
+# rpmdev-setuptree
+
+# VERSION="0.1"
+
+# mv source_dir/src cros3-${VERSION}
+# tar cjf cros3-${VERSION}.tar.bz2 cros3-${VERSION}
+# cp *.tar.bz2 ~/rpmbuild/SOURCES/
+# cp source_dir/*.spec .
+
+# dnf builddep -y cros3.spec
+
+# rpmbuild -ba cros3.spec
+
+# cd ~
+# find
+# ls -la
+
+# mkdir -p ${GITHUB_WORKSPACE}/rpm-artifacts
+# cp -r ~/rpmbuild/RPMS ${GITHUB_WORKSPACE}/rpm-artifacts/
+# cp -r ~/rpmbuild/SRPMS ${GITHUB_WORKSPACE}/rpm-artifacts/
+
+
+dnf install -y gcc rpm-build rpm-devel rpmlint make bash diffutils patch rpmdevtools
 rpmdev-setuptree
 
-VERSION="0.1"
-
-mv source_dir/src cros3-${VERSION}
-tar cjf cros3-${VERSION}.tar.bz2 cros3-${VERSION}
+mv source_dir/src cros3-0.1
+tar cjf cros3-0.1.tar.bz2 cros3-0.1
 cp *.tar.bz2 ~/rpmbuild/SOURCES/
 cp source_dir/*.spec .
 
@@ -23,7 +44,3 @@ rpmbuild -ba cros3.spec
 cd ~
 find
 ls -la
-
-mkdir -p ${GITHUB_WORKSPACE}/rpm-artifacts
-cp -r ~/rpmbuild/RPMS ${GITHUB_WORKSPACE}/rpm-artifacts/
-cp -r ~/rpmbuild/SRPMS ${GITHUB_WORKSPACE}/rpm-artifacts/
