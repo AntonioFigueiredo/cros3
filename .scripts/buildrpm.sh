945d4e2694b41599e92233d4c282c641eacf87e9
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-05-07 20:59:04 +0200

    Switch package manager from yum to dnf in buildrpm script

--- .scripts/buildrpm.sh
+++ .scripts/buildrpm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
-
-yum install -y epel-release
+set -e
+dnf install -y epel-release
 
 KERNEL_VER=$(uname -r)
-yum install -y "kernel-devel-${KERNEL_VER}"
-yum install -y gcc make rpm-build rpmdevtools dkms yum-utils kernel-abi-stablelists kernel-rpm-macros udev
+dnf install -y "kernel-devel-${KERNEL_VER}"
+dnf install -y gcc make rpm-build rpmdevtools dkms yum-utils kernel-abi-stablelists kernel-rpm-macros udev
 
 rpmdev-setuptree
 
@@ -15,7 +15,7 @@ tar cjf cros3-${VERSION}.tar.bz2 cros3-${VERSION}
 cp *.tar.bz2 ~/rpmbuild/SOURCES/
 cp source_dir/*.spec .
 
-yum-builddep -y cros3.spec
+dnf-builddep -y cros3.spec
 
 rpmbuild -ba cros3.spec
 
