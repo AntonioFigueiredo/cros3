9e744c69181bc0c1b35b5b9d4112ae8f976a70e8
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-05-07 21:53:39 +0200

    test

--- .scripts/buildrpm.sh
+++ .scripts/buildrpm.sh
@@ -15,6 +15,7 @@ mv source_dir/src cros3-${VERSION}
 tar -cvjf cros3-${VERSION}.tar.bz2 cros3-${VERSION}
 cp *.tar.bz2 ~/rpmbuild/SOURCES/
 cp source_dir/*.spec .
+cp cros3.spec "$RPMBUILD_DIR/SPECS/"
 
 dnf builddep -y cros3.spec
 
