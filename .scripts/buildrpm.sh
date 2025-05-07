9719b35aefc514b4e42d97066c1bd403f0753a30
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-05-07 22:22:18 +0200

    test

--- .scripts/buildrpm.sh
+++ .scripts/buildrpm.sh
@@ -15,11 +15,14 @@ mv source_dir/src cros3-${VERSION}
 tar -cvjf cros3-${VERSION}.tar.bz2 cros3-${VERSION}
 cp *.tar.bz2 ~/rpmbuild/SOURCES/
 cp source_dir/*.spec .
-#cp cros3.spec "$RPMBUILD_DIR/SPECS/"
 
 dnf builddep -y cros3.spec
 
 KERNEL_VERSION=$(uname -r)
+echo "DEBUG: installed kernel-devel:"
+rpm -q kernel-devel
+KERNEL_VERSION=$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-devel)
+echo "DEBUG: Using KERNEL_VERSION=$KERNEL_VERSION"
 rpmbuild -ba cros3.spec --define "kernel_version ${KERNEL_VERSION}"
 
 #cd ~
