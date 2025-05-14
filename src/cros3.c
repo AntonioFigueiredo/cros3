1d8711d98ebd1eea0d12fae662c5952d71d0550a
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-05-14 18:14:57 +0200

    Fix class creation condition for cros3 module initialization

--- src/cros3.c
+++ src/cros3.c
@@ -295,7 +295,7 @@ static int __init cros3_init_module (void)
 	}
 	cros3_major = MAJOR(devreg);
 
-	#if defined(RHEL_KERNEL)
+	#if defined(class_create) && !defined(CONFIG_CLASS_CREATE_TAKES_MODULE)
     	cros3_class = class_create("cros3");
 	#else
     	cros3_class = class_create(THIS_MODULE, "cros3");
